e67af3e65f83a6518dfd69c12757a56fa73be37c
Natirx <triko2020@gmail.com>
2017-08-16 20:34:24 +0300

    Redesing main page

---
+++ app/models/project.rb
@@ -0,0 +1,7 @@
+class Project < ApplicationRecord
+
+    extend FriendlyId
+    friendly_id :title, use: :slugged
+    has_attached_file :picture, styles: { large: "700x700>", medium: "300x300>", thumb: "150x150#" }
+    validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+end
